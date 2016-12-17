5fd016d809442809146168da85ad611c777fa77a
Mrcrunch91 <alterac91@yahoo.com>
2016-12-17 06:34:37 -0500

    Replace files for kims salon.

---
+++ kimFun.js
@@ -0,0 +1,42 @@
+/*Javascript and jQuery */
+
+//Set variable house to get current local date. 
+var house = new Date();
+var theH = house.getHours();
+house.setHours(theH);
+//Var array for months of the year.
+var theMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", 
+"November", "December"];
+
+//Var array for days of the week.
+var theday = ["Sun","Mon ","Tue ", "Wed ", "Thur ", "Fri ", "Sat "];
+var theNum = house.getDay();
+
+
+var inputMonth = (theMonths[house.getMonth()]);
+
+//Variable string for footer date time.
+var toSend = ("Today is: " + theday[theNum] + " " + inputMonth + " "
+	+ house.getDate() + " " + house.getFullYear()) + " " + house.getHours() + ":" + house.getMinutes() + ":" + house.getSeconds() 
+	+ " GMT-0500 (Eastern Standard Time)";
+
+//Display date inside of css div "east".
+document.getElementById("east").innerHTML = toSend;
+
+//Highlights function for forms and tables data. Yellow for table row and red for table data. 
+$(document).ready(function(){
+	$('table tbody tr').hover(function(){
+		$(this).addClass('highlight1');		
+	}, function(){
+		$(this).removeClass('highlight1');
+		
+	});
+
+});
+
+//for Arrival time calander pick to implement in HTML pages. 
+$(function(){
+	$(".myArrival").datepicker();
+});
+//Placeholder for arrival time form row.
+document.getElementById("myArrival").placeholder = "mm/dd/yyyy";
