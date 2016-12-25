b86817e1876a0d23cc74c16c3d3b1eeb2b680c3c
Mrcrunch91 <alterac91@yahoo.com>
2016-12-25 03:19:26 -0500

    Changes for favicon fixes.

--- kimFun.js
+++ kimFun.js
@@ -61,16 +61,14 @@ function initMap(){
 
 
 $(document).ready(function () {
-    var url = window.location.href;
-   
+	var url = window.location.href;
+	var activePage = url;
+	$("a").each(function () {
+		var linkPage = this.href;
 
-    var activePage = url;
-    $("a").each(function () {
-        var linkPage = this.href;
-
-        if (activePage == linkPage) {
-            $(this).closest("li").addClass("current");
-        }
-    });
+		if (activePage == linkPage) {
+			$(this).closest("li").addClass("current");
+		}
+	});
 });
 
